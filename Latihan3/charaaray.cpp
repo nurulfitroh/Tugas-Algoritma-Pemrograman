54be4ea5800a5f2b47dfb7b63142c7f30cc35088
nurulfitroh <nurulfitroh.a@gmail.com>
2017-02-20 12:35:38 +0700

    Pertemuan 6,7,8
    Pertemuan 6,7,8

---
+++ Latihan3/charaaray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+// Program Fungsi Char Array
+// I.S Program mengolah angka
+// F.S Program Menampilkan Data Array
+
+int main() {
+    // Fungsi string
+    string nama = "NURULFITROH" ;
+    // Fungsi Char
+    char nama2[] =  "NURULFITROH" ;
+    char nama3[11] = {'N', 'U', 'R', 'U', 'L', 'F', 'I', 'T', 'R', 'O', 'H'};
+    // Menampilkan hasil data
+    cout << nama << endl;
+    cout << nama2 << endl;
+    cout << nama3[5] << endl; 
+
+// Akhir Program
+return 0;
+}
